f7e83a1825873c927ad99e32dc018d9fa3dc11d3
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2020-04-23 14:17:11 +0200

    Styling

--- resources/views/tile.blade.php
+++ resources/views/tile.blade.php
@@ -1,11 +1,13 @@
 <x-dashboard-tile :position="$position">
-    <div wire:poll.{{ $refreshInSeconds }}s class="grid gap-padding h-full markup">
-        <ul class="align-self-center">
+    <div wire:poll.{{ $refreshInSeconds }}s class="grid h-full">
+        <ul class="self-center divide-y-2 divide-canvas">
             @foreach($events as $event)
-                <li>
+                <li class="py-1">
                     <div class="my-2">
                         <div class="{{ $event['withinWeek'] ? 'font-bold' : '' }}">{{ $event['name'] }}</div>
-                        <div class="text-sm text-dimmed">{{ $event['date']->format('d.m.Y') }}</div>
+                        <div class="text-sm text-dimmed">
+                            {{ $event['date']->format('d.m.Y') }}
+                        </div>
                     </div>
                 </li>
             @endforeach
